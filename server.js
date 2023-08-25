3512c460da9b04aeed5d63881571906bf58e94b0
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-24 20:23:19 -0400

    set up server to see the front page and  style

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Heroku application name morning-journey-68779
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up Handlebars.js engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Basic route to render the main.handlebars
+app.get('/', (req, res) => {
+    res.render('index'); // Assuming 'index.handlebars' is the name of your main file
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
