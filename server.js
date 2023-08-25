7b4e448db11f3261f7554b157bfb7b29958069be
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-24 20:46:01 -0400

    upload server and correct root style

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 
-const app = express();
+const app = express();  // Initialize the Express app first
 
-const PORT = process.env.PORT || 3000;  // <-- Define the PORT variable here
+const PORT = process.env.PORT || 3000;
 
 const hbs = handlebars.create({
     defaultLayout: 'main',
@@ -14,15 +14,14 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static('public'));  // Place this line after initializing the app
 
 // Basic route to render the main.handlebars
 app.get('/', (req, res) => {
-    res.render('index'); // Assuming 'index.handlebars' is the name of your main file
+    res.render('index');
 });
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
