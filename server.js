3ee931c3f4396018fe3c73539ec5f63f515d0136
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-28 19:44:55 -0400

    Changes were made

--- server.js
+++ server.js
@@ -39,10 +39,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-const forceDb = process.env.FORCE_DB === 'true'; // Use an environment variable
 
-sequelize.sync({ force: forceDb }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
