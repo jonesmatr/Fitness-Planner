d0c5b93f86056278bbe5bc533b0439fdf006eb05
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-28 20:27:34 -0400

    Uncomented routes in server.

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
