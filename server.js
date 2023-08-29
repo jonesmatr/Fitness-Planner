1b177cb39c567b83a9856e4c96fb72b8250a8376
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-29 17:42:40 -0400

    update server from main

--- server.js
+++ server.js
@@ -1,37 +1,67 @@
+const path = require('path');
 const express = require('express');
-const handlebars = require('express-handlebars');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+const { format_date } = require('./utils/helpers');
+const homeRoutes = require('./controllers/api/homeRoutes');
+const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();  // Initialize the Express app first
+// Log the current directory
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const hbs = handlebars.create({
-    defaultLayout: 'main',
-    extname: '.handlebars'
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ 
+  helpers: {
+    format_date
+  },
+  defaultLayout:'homepage',
+  partialsDir: path.join(__dirname, 'views/partials/')
 });
 
+// Explicitly set the views directory
+// app.set('views', path.join(__dirname, 'views'));
+
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));  // Place this line after initializing the app
+// app.set('views', './views');
 
-// Basic route to render the main.handlebars
-app.get('/login', (req, res) => {
-    res.render('login');
-});
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
+// Activate specific routes
+// app.use('/', homeRoutes);
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// // Activate routes
+app.use(routes);
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Error handling middleware (Add this part)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
